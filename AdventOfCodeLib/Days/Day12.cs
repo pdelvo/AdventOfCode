8090e887f01b4a8e411f32bbde0dcc5f9fb28ac5
Dennis Fischer <fischer_dennis@live.de>
2023-12-11 13:23:18 +0100

    Prepare day 12

---
+++ AdventOfCodeLib/Days/Day12.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace AdventOfCodeLib.Days
+{
+    public class Day12 : Day
+    {
+        public override string? Description => "Test";
+        public override string TestInput1 => @"";
+        public override string TestOutput1 => "Test";
+
+        public override string TestOutput2 => "Test";
+        public override string RunPart1()
+        {
+            return "";
+        }
+
+        public override string RunPart2()
+        {
+            return "";
+        }
+    }
+}
