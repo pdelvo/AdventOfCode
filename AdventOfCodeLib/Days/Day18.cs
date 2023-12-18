0ffeaa077f01bf005529688beb099ee8913ce678
Dennis Fischer <fischer_dennis@live.de>
2023-12-18 23:14:36 +0100

    It works!

--- AdventOfCodeLib/Days/Day18.cs
+++ AdventOfCodeLib/Days/Day18.cs
@@ -32,140 +32,150 @@ U 2 (#7a21e3)";
 
         public override string RunPart2()
         {
-            return "";
             return Compute(true).ToString();
         }
 
         private string Compute(bool isPart2)
         {
-            checked
+            LinkedList<Instruction> instructions = new();
+            BigInteger insideCounter = 0;
+
+            for (int i = 0; i < Lines.Length; i++)
             {
-                List<(int x, int y, Turn turn)> points = [(0, 0, Turn.None)];
-                LinkedList<Instruction> instructions = new();
-                BigInteger insideCounter = 0;
+                Instruction instruction = ParseInstruction(Lines[i], isPart2);
+                instructions.AddLast(instruction);
+            }
 
-                for (int i = 0; i < Lines.Length; i++)
-                {
-                    Instruction instruction = ParseInstruction(Lines[i], isPart2);
-                    instructions.AddLast(instruction);
-                }
-
+            while (instructions.Count > 4)
+            {
+                Console.WriteLine(instructions.Count);
                 var instruction0Node = instructions.First!;
 
-                for (int i = 0; ; i++)
+                int minDistance = int.MaxValue;
+                LinkedListNode<Instruction>? minNode = null;
+                for (int i = 0; i < instructions.Count; i++)
                 {
-                    if (instructions.Count == 4)
-                    {
-                        insideCounter += ((BigInteger)instructions.First!.Value.NumberOfSteps + 1) * ((BigInteger)instructions.First.Next!.Value.NumberOfSteps + 1);
-                        break;
-                    }
+                    var inst1Node = GetNextCycle(instruction0Node);
+                    var inst2Node = GetNextCycle(inst1Node);
+                    var inst3Node = GetNextCycle(inst2Node);
+                    var inst4Node = GetNextCycle(inst3Node);
+                    Instruction inst0 = instruction0Node.Value;
+                    Instruction inst1 = inst1Node.Value;
+                    Instruction inst2 = inst2Node.Value;
+                    Instruction inst3 = inst3Node.Value;
+                    Instruction inst4 = inst4Node.Value;
 
-                    var instruction1Node = GetNextCycle(instruction0Node);
-                    var instruction2Node = GetNextCycle(instruction1Node);
-                    var instruction3Node = GetNextCycle(instruction2Node);
-                    var instruction4Node = GetNextCycle(instruction3Node);
-                    Instruction instruction0 = instruction0Node.Value;
-                    Instruction instruction1 = instruction1Node.Value;
-                    Instruction instruction2 = instruction2Node.Value;
-                    Instruction instruction3 = instruction3Node.Value;
-                    Instruction instruction4 = instruction4Node.Value;
+                    var t0 = GetTurnType(inst0, inst1);
+                    var t1 = GetTurnType(inst1, inst2);
+                    var t2 = GetTurnType(inst2, inst3);
+                    var t3 = GetTurnType(inst3, inst4);
 
-                    var turn0 = GetTurnType(instruction0, instruction1);
-                    var turn1 = GetTurnType(instruction1, instruction2);
-                    var turn2 = GetTurnType(instruction2, instruction3);
-                    var turn3 = GetTurnType(instruction3, instruction4);
-
-                    if (turn0 == turn1 && turn1 == turn2 && turn2 == turn3)
+                    if (t0 == t1 && t1 == t2 && t2 == t3)
                     {
                         // Scary case. Don't want to. Find something else
-                        continue;
                     }
-
-                    if (turn1 == turn2)
+                    else if (t1 == t2)
                     {
-                        long shorterSide;
-                        if (instruction1.NumberOfSteps == instruction3.NumberOfSteps)
+                        if (inst2.NumberOfSteps < minDistance)
                         {
-                            shorterSide = instruction1.NumberOfSteps;
-                            instruction0Node.ValueRef = instruction0 with { NumberOfSteps = instruction0.NumberOfSteps + instruction2.NumberOfSteps + instruction4.NumberOfSteps };
-
-                            instructions.Remove(instruction1Node);
-                            instructions.Remove(instruction2Node);
-                            instructions.Remove(instruction3Node);
-                            instructions.Remove(instruction4Node);
-                            if (instruction2.Direction != instruction0.Direction)
-                            {
-                                insideCounter -= instruction0.NumberOfSteps;
-                            }
-                            if (instruction2.Direction != instruction4.Direction)
-                            {
-                                insideCounter -= instruction4.NumberOfSteps;
-                            }
-                        }
-                        else if (instruction1.NumberOfSteps < instruction3.NumberOfSteps)
-                        {
-                            shorterSide = instruction1.NumberOfSteps;
-
-                            instruction0Node.ValueRef = instruction0 with
-                            {
-                                Direction = instruction2.Direction,
-                                NumberOfSteps = instruction2.NumberOfSteps + (instruction2.Direction == instruction0.Direction ? instruction0.NumberOfSteps : -instruction0.NumberOfSteps)
-                            };
-
-                            instructions.Remove(instruction1Node);
-                            instructions.Remove(instruction2Node);
-
-                            instruction3Node.ValueRef = instruction3 with { NumberOfSteps = instruction3.NumberOfSteps - instruction1.NumberOfSteps };
-
-
-                            if (instruction2.Direction != instruction0.Direction)
-                            {
-                                insideCounter += instruction0.NumberOfSteps;
-                            }
-                        }
-                        else
-                        {
-                            shorterSide = instruction3.NumberOfSteps;
-                            instruction4Node.ValueRef = instruction4 with
-                            {
-                                Direction = instruction2.Direction,
-                                NumberOfSteps = instruction2.NumberOfSteps + (instruction2.Direction == instruction4.Direction ? instruction4.NumberOfSteps : -instruction4.NumberOfSteps)
-                            };
-
-                            instructions.Remove(instruction3Node);
-                            instructions.Remove(instruction2Node);
-
-                            instruction1Node.ValueRef = instruction1 with { NumberOfSteps = instruction1.NumberOfSteps - instruction3.NumberOfSteps };
-
-
-                            if (instruction2.Direction != instruction4.Direction)
-                            {
-                                insideCounter += instruction4.NumberOfSteps;
-                            }
-                        }
-
-                        if (turn1 == Turn.Left)
-                        {
-                            insideCounter -= (BigInteger)shorterSide * ((BigInteger)instruction2.NumberOfSteps - 1);
-                        }
-                        else
-                        {
-                            insideCounter += (BigInteger)shorterSide * ((BigInteger)instruction2.NumberOfSteps + 1);
+                            minNode = instruction0Node;
+                            minDistance = inst2.NumberOfSteps;
                         }
                     }
-                    else
+                    instruction0Node = inst1Node;
+                }
+                instruction0Node = minNode!;
+                var instruction1Node = GetNextCycle(instruction0Node);
+                var instruction2Node = GetNextCycle(instruction1Node);
+                var instruction3Node = GetNextCycle(instruction2Node);
+                var instruction4Node = GetNextCycle(instruction3Node);
+                Instruction instruction0 = instruction0Node.Value;
+                Instruction instruction1 = instruction1Node.Value;
+                Instruction instruction2 = instruction2Node.Value;
+                Instruction instruction3 = instruction3Node.Value;
+                Instruction instruction4 = instruction4Node.Value;
+
+                var turn0 = GetTurnType(instruction0, instruction1);
+                var turn1 = GetTurnType(instruction1, instruction2);
+                var turn2 = GetTurnType(instruction2, instruction3);
+                var turn3 = GetTurnType(instruction3, instruction4);
+
+                long shorterSide;
+                if (instruction1.NumberOfSteps == instruction3.NumberOfSteps)
+                {
+                    shorterSide = instruction1.NumberOfSteps;
+                    instruction0Node.ValueRef = instruction0 with { NumberOfSteps = instruction0.NumberOfSteps + instruction2.NumberOfSteps + instruction4.NumberOfSteps };
+
+                    instructions.Remove(instruction1Node);
+                    instructions.Remove(instruction2Node);
+                    instructions.Remove(instruction3Node);
+                    instructions.Remove(instruction4Node);
+                    if (instruction2.Direction != instruction0.Direction)
                     {
-                        instruction0Node = instruction1Node;
+                        insideCounter -= instruction0.NumberOfSteps;
                     }
+                    if (instruction2.Direction != instruction4.Direction)
+                    {
+                        insideCounter -= instruction4.NumberOfSteps;
+                    }
+                }
+                else if (instruction1.NumberOfSteps < instruction3.NumberOfSteps)
+                {
+                    shorterSide = instruction1.NumberOfSteps;
 
-                    // Debug:
-                    var fillSolution = FillAlgorithm(instructions) + insideCounter;
+                    instruction0Node.ValueRef = instruction0 with
+                    {
+                        Direction = instruction2.Direction,
+                        NumberOfSteps = instruction2.NumberOfSteps + (instruction2.Direction == instruction0.Direction ? instruction0.NumberOfSteps : -instruction0.NumberOfSteps)
+                    };
+
+                    instructions.Remove(instruction1Node);
+                    instructions.Remove(instruction2Node);
+
+                    instruction3Node.ValueRef = instruction3 with { NumberOfSteps = instruction3.NumberOfSteps - instruction1.NumberOfSteps };
 
 
+                    if (instruction2.Direction != instruction0.Direction)
+                    {
+                        insideCounter += instruction0.NumberOfSteps;
+                    }
+                }
+                else
+                {
+                    shorterSide = instruction3.NumberOfSteps;
+                    instruction4Node.ValueRef = instruction4 with
+                    {
+                        Direction = instruction2.Direction,
+                        NumberOfSteps = instruction2.NumberOfSteps + (instruction2.Direction == instruction4.Direction ? instruction4.NumberOfSteps : -instruction4.NumberOfSteps)
+                    };
+
+                    instructions.Remove(instruction3Node);
+                    instructions.Remove(instruction2Node);
+
+                    instruction1Node.ValueRef = instruction1 with { NumberOfSteps = instruction1.NumberOfSteps - instruction3.NumberOfSteps };
+
+
+                    if (instruction2.Direction != instruction4.Direction)
+                    {
+                        insideCounter += instruction4.NumberOfSteps;
+                    }
                 }
 
-                return insideCounter.ToString();
+                if (turn1 == Turn.Left)
+                {
+                    insideCounter -= (BigInteger)shorterSide * ((BigInteger)instruction2.NumberOfSteps - 1);
+                }
+                else
+                {
+                    insideCounter += (BigInteger)shorterSide * ((BigInteger)instruction2.NumberOfSteps + 1);
+                }
+
+                // Debug:
+                //var fillSolution = FillAlgorithm(instructions) + insideCounter;
             }
+            insideCounter += ((BigInteger)instructions.First!.Value.NumberOfSteps + 1) * ((BigInteger)instructions.First.Next!.Value.NumberOfSteps + 1);
+
+            return insideCounter.ToString();
         }
 
         private int FillAlgorithm(LinkedList<Instruction> instructions)
